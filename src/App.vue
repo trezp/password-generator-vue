3b84be30bac8c4775f23ffc34b8582f9ccc88e8c
Treasure Porth <treasure@scrimba.com>
2022-09-26 17:59:34 -0700

    moved data into it's own file, deleted starter component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script>
 
 import PasswordBox from './components/PasswordBox.vue';
+import characters from './characters.js';
 
 export default {
   components: {
@@ -8,19 +9,18 @@ export default {
   },
   data() {
     return {
-      characters: ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"],
       password1: '',
       password2: ''
     }
   },
   methods: {
-    getRandomCharacter(max){
-      return this.characters[Math.floor(Math.random() * max)]; 
+    getRandomCharacter(arr, max){
+      return arr[Math.floor(Math.random() * max)]; 
     },
     generatePassword(charLength){
       let password = '';
       for(let i = 0; i < charLength; i++){
-        password += this.getRandomCharacter(this.characters.length);
+        password += this.getRandomCharacter(characters, this.characters.length);
       }
       return password; 
     },
